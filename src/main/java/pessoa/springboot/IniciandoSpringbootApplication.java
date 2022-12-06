5d0a7c945cef93fa77fdc2e572836123ea0eaf02
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-06 10:25:26 -0400

    Introdução ao Spring Framework com Spring Boot

---
+++ src/main/java/pessoa/springboot/IniciandoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package pessoa.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciandoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniciandoSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pessoa/springboot/IniciandoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pessoa.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciandoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
