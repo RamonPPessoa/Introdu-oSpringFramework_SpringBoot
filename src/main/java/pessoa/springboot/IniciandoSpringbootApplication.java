8e7789246d5a1b700a466efa0a5fa66d20401f16
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2022-12-06 17:57:10 -0400

    Conhecendo a estrtura, configuração inicial de um projeto spring boot

--- src/main/java/pessoa/springboot/IniciandoSpringbootApplication.java
+++ src/main/java/pessoa/springboot/IniciandoSpringbootApplication.java
@@ -1,5 +1,6 @@
 package pessoa.springboot;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IniciandoSpringbootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IniciandoSpringbootApplication.class, args);
+
+
 	}
 
 }
---
+++ src/main/java/pessoa/springboot/MyApp.java
@@ -0,0 +1,18 @@
+package pessoa.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApp implements CommandLineRunner {
+    @Autowired
+    private Calculadora calc;
+    @Override
+    public void run(String... args) throws Exception {
+
+
+        System.out.println("Resultado é: " +calc.somar(5,5));
+
+    }
+}
